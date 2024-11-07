3dbefbb7b4130978ec3527aeebc9200bcca6b63c
12lolo <145444658+12lolo@users.noreply.github.com>
2024-11-07 12:04:55 +0100

    test

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 return [
     'smtp_host' => 'smtp.hostinger.com',
     'smtp_user' => 'info@sennevisser.me', // Your Hostinger email address
-    'smtp_pass' => 'Viskampioen3#', // Your Hostinger email password (consider using environment variables instead)
+    'smtp_pass' => getenv('SMTP_PASS'), // Use the environment variable
     'smtp_port' => 465, // Hostinger uses port 465 for SSL
     'from_email' => 'info@sennevisser.me', // Your Hostinger email address
     'from_name' => 'Senne_Visser', // Your name or website name
